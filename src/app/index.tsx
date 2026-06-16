f2f3921492a76d64cd55b51cde3d92fae669b94c
darius <keto543@gmail.com>
2026-06-16 11:40:02 +0530

    added native wind with final implementation of tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,9 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={styles.container}>
-      <Text>Hello screen is working fine, is it ?</Text>
-      <Link href="/about">About</Link>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-600 text-xl font-bold">Hello from NativeWind</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
